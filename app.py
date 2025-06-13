784182a3bf74cdea8650a2de9c5e6e3377b3233f
MohamedOsman30 <125395428+MohamedOsman30@users.noreply.github.com>
2025-06-13 14:30:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -86,4 +86,5 @@ def predict():
             print("Temporary image file removed.")
 
 if __name__ == '__main__':
-    app.run(host='https://modelai.up.railway.app/', port=8080)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
