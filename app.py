2981ca288454c2140add08dccc6358c53b48ae6e
MohamedOsman30 <125395428+MohamedOsman30@users.noreply.github.com>
2025-06-13 14:40:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -85,6 +85,4 @@ def predict():
             os.remove(image_path)
             print("Temporary image file removed.")
 
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+
